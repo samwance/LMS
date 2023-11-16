e9f8778f4bdf263b6016d11191a44955114d86da
semyon <odintsovvans@gmail.com>
2023-11-17 05:17:09 +0600

    26_1 urls for course, lesson payments

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from courses.apps import CoursesConfig
 from courses.views import LessonListAPIView, LessonCreateAPIView, CourseViewSet, LessonUpdateAPIView, \
     LessonDestroyAPIView, LessonRetrieveAPIView, PaymentListAPIView, SubscriptionCreateAPIView, \
-    SubscriptionDestroyAPIView
+    SubscriptionDestroyAPIView, LessonPaymentAPIView, CoursePaymentAPIView
 from rest_framework.routers import DefaultRouter
 
 app_name = CoursesConfig.name
@@ -18,7 +18,9 @@ urlpatterns = [
     path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
     path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
-    path('payment/', PaymentListAPIView.as_view(), name='payment'),
+    path('payments/', PaymentListAPIView.as_view(), name='payments'),
+    path('courses/<int:pk>/pay/', CoursePaymentAPIView.as_view(), name='course_pay'),
+    path('lessons/<int:pk>/pay/', LessonPaymentAPIView.as_view(), name='lesson_pay'),
     path('courses/<int:pk>/create_sub/', SubscriptionCreateAPIView.as_view(), name='sub_create'),
     path('courses/<int:pk>/delete_sub/', SubscriptionDestroyAPIView.as_view(), name='sub_delete'),
 ] + router.urls
